14a69aaefed4feb10bab9e6dcf7dda0c5c8584b1
Kacper Torba <ka.toorba@gmail.com>
2023-04-04 10:55:08 +0200

    Add and edit posts

--- blog/__init__.py
+++ blog/__init__.py
@@ -4,16 +4,17 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config.from_object('config.Config')
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from blog import routes, models
 
 
+from blog import models, routes
 
-
-'''config = configparser.ConfigParser()
-    config.read(
-        os.path.join(os.path.dirname(
-            os.path.abspath(__file__)), fileName))'''
+@app.shell_context_processor
+def make_shell_context():
+  return {
+      "db": db,
+      "Entry": models.Entry
+  }
--- blog/routes.py
+++ blog/routes.py
@@ -1,14 +1,58 @@
-from flask import Flask, render_template
-#from blog import app problems with import !!!
+from flask import render_template, request, flash, redirect, url_for
+from blog import app
+from .models import Entry, db
+from .forms import EntryForm
 
 
-app = Flask(__name__)
 
 @app.route('/')
-def index():    
-    return render_template("base.html")
+def index():
+    all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
+    
+    return render_template("homepage.html",all_posts=all_posts)
 
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/new-post/", methods=["GET", "POST"])
+@app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+def add_edit_entry(entry_id=None):
+    errors = None
+    form = EntryForm()
+    flag = 1
+    
+    if entry_id == None and request.method == 'POST':
+        if form.validate_on_submit():
+            entry = Entry(
+               title = form.title.data,
+               body = form.body.data,
+               is_published = form.is_published.data
+           )
+            db.session.add(entry)
+            db.session.commit()
+            
+            if form.is_published.data:
+                flash(f'Post was successfully add.')
+            else:
+                flash(f'Post was successfully add but not published.')
+                
+            return redirect(url_for('index'))
+            
+        else:
+            errors = form.errors
+            flash(f'Error: {errors}')              
+    
+    elif entry_id:        
+        flag = 0
+        entry = Entry.query.filter_by(id=entry_id).first_or_404()
+        form = EntryForm(obj=entry)
+        if request.method == 'POST':
+            if form.validate_on_submit():
+                form.populate_obj(entry)
+                db.session.commit()                
+                
+                return redirect(url_for('index'))
+        
+            else:
+                errors = form.errors
+                flash(f'Error: {errors}')            
+                        
+    return render_template("entry_form.html", form=form, errors=errors, flag = flag)
